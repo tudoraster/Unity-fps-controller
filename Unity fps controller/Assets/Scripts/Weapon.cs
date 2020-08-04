351b3255ad2bdbcb75bee3040b40af09f5c571b1
Tudor <tudormatei010@gmail.com>
2020-08-04 10:20:30 +0300

    Added animations and ammo

--- Unity fps controller/Assets/Scripts/Weapon.cs
+++ Unity fps controller/Assets/Scripts/Weapon.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Weapon : MonoBehaviour
 {
@@ -10,34 +11,85 @@ public class Weapon : MonoBehaviour
     public ParticleSystem muzzleFlash;
     public ParticleSystem smokeAfterMuzzleFlash;
     public Animator animator;
+    public Text ammoInfo;
 
     //Setting gun stats
     public float damage = 10f;
     public float range = 100f;
-    private bool HasShot = false;
+    public float fireRate = 15f;
+
+    //Ammo settings
+    private int maxAmmo = 7;
+    private int currentAmmo;
+    private float reloadTime = 1f;
+    private bool CanReload = false;
+    private bool isReloading = false;
+
+    private float nextTimeToFire = 0f;
+
+    private void Start()
+    {
+        ammoInfo.text = "7 / 7";
+        currentAmmo = maxAmmo;
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("CanReload", false);
+    }
 
     private void Update()
     {
-        HasShot = false;
+        if (isReloading)
+            return;
+
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+        }
 
         //Checking for user input on left mouse button.
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
         {
-            HasShot = true;
+            nextTimeToFire = Time.time + 1f/fireRate;
 
-            animator.SetBool("HasShot", HasShot);
             Shoot();
         }
+        else { animator.SetBool("HasShot", false); }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+
+        animator.SetBool("CanReload", true);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        animator.SetBool("CanReload", false);
+
+        currentAmmo = maxAmmo;
+
+        ammoInfo.text = "7 / 7";
+
+        isReloading = false;
     }
 
     private void Shoot()
     {
+        //Everyhit the ammo is 
+        currentAmmo--;
+        ammoInfo.text = currentAmmo.ToString() + " / 7";
+
         //Defining raycast variable
         RaycastHit hit;
 
         muzzleFlash.Play();
 
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        animator.SetBool("HasShot", true);
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
         }
